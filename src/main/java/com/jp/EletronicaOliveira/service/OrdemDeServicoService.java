d8af7d46deecb0d4e79a2002da6d5e7c9e96cb5b
joaopedrooliveir4 <jp3066984@gmail.com>
2025-04-08 20:01:28 -0300

    feat: adiciona métodos de busca por id, nome, telefone e aparelho no serviço. Implementados métodos para recuperar ordens de serviço com base em diferentes critérios.

--- src/main/java/com/jp/EletronicaOliveira/service/OrdemDeServicoService.java
+++ src/main/java/com/jp/EletronicaOliveira/service/OrdemDeServicoService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrdemDeServicoService {
@@ -16,5 +17,25 @@ public class OrdemDeServicoService {
      public List<OrdemDeServico> listarTodas() {
          // Esse método pega todas as ordens de serviço salvas
          return ordemDeServicoRepository.findAll();
+     }
+
+     // Nessa parte filtramos nossas Ordens com base em: id, nome, telefone e aparelho
+     public Optional<OrdemDeServico> buscarOrdemPorId(Long id) {
+         // O objeto Optional diferente de List evita o erro caso o retorno seja null,
+         // sendo assim esse método pode ou não me retornar algo evitando a exeção
+         return ordemDeServicoRepository.findById(id);
+     }
+
+     // Os métodos list vão me retornar uma lista com cada parâmetro igual
+     public List<OrdemDeServico> buscarOrdemPorNome(String nome) {
+         return ordemDeServicoRepository.findByNomeContainingIgnoreCase(nome);
+     }
+
+     public List<OrdemDeServico> buscarOrdemPorTelefone(String telefone) {
+         return ordemDeServicoRepository.findByTelefone(telefone);
+     }
+
+     public List<OrdemDeServico> buscarOrdemPorAparelho(String aparelho) {
+         return ordemDeServicoRepository.findByAparelhoContainingIgnoreCase(aparelho);
      }
 }
