4387a842e56258d7b11163603495b9ff9801f69a
joaopedrooliveir4 <jp3066984@gmail.com>
2025-04-08 21:01:58 -0300

    test: testando endpoints GET e POST

---
+++ src/main/java/com/jp/EletronicaOliveira/EletronicaOliveiraApplication.java
@@ -0,0 +1,13 @@
+package com.jp.EletronicaOliveira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletronicaOliveiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletronicaOliveiraApplication.class, args);
+	}
+
+}
